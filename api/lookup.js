abe12dcf78a89c5033035d7090d115e5184dce90
VinzzXD24 <167677323+VinzzXD24@users.noreply.github.com>
2025-04-10 22:27:21 +0700

    Update lookup.js

--- api/lookup.js
+++ api/lookup.js
@@ -1,27 +1,24 @@
 // api/lookup.js
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const { cardNumber } = req.query;
-  
+
   if (!cardNumber) {
     return res.status(400).json({ error: 'Parameter cardNumber tidak disediakan.' });
   }
-  
+
   try {
     const apiUrl = `https://lookup.binlist.net/${cardNumber}`;
     const response = await fetch(apiUrl, {
-      headers: {
-        'Accept-Version': '3'
-      }
+      headers: { 'Accept-Version': '3' }
     });
-    
+
     if (!response.ok) {
       return res.status(response.status).json({ error: response.statusText });
     }
-    
+
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: error.toString() });
+    res.status(500).json({ error: error.message });
   }
-};
+}
